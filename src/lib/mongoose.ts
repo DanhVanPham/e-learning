abd51fa4ad6bcb860a7e03525bb2f4a066522eda
DanhVanPham <Danhskipper18@gmail.com>
2024-07-18 22:53:50 +0700

    Config connection database

---
+++ src/lib/mongoose.ts
@@ -0,0 +1,25 @@
+'use server'
+
+import mongoose from 'mongoose'
+
+// singleton connection
+let isConnected: boolean = false
+
+export const connectToDatabase = async () => {
+  if (!process.env.MONGO_DB_URL) throw new Error('MONGO_DB_URL is not set')
+
+  if (isConnected) {
+    console.log('MongoDB is already connected')
+    return
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGO_DB_URL, {
+      dbName: 'Ucademy',
+    })
+    isConnected = true
+    console.log('Using new database connection')
+  } catch (error) {
+    console.log('Error while connecting database: ', error)
+  }
+}
