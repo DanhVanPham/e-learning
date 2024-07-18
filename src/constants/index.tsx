6df79ffee5330c7f7efbed373d90cb251978ff5e
DanhVanPham <Danhskipper18@gmail.com>
2024-07-18 22:55:14 +0700

    Setup global icons

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -1,16 +1,43 @@
 import React from 'react'
-import { IconPlay, IconExplore } from '@/components/icons'
+import {
+  IconPlay,
+  IconExplore,
+  IconOrder,
+  IconComment,
+  IconUsers,
+  IconStudy,
+} from '@/components/icons'
 import type { TMenuItem } from '@/types/index.t'
 
 export const menuItems: TMenuItem[] = [
   {
-    title: 'Khu vực học tập',
+    title: 'Khám phá',
     url: '/',
+    icon: <IconExplore className="size-5" />,
+  },
+  {
+    title: 'Khu vực học tập',
+    url: '/study',
+    icon: <IconStudy className="size-5" />,
+  },
+  {
+    title: 'Quản lý khóa học',
+    url: '/manage/course',
     icon: <IconPlay className="size-5" />,
   },
   {
-    title: 'Khám phá',
-    url: '/explore',
-    icon: <IconExplore className="size-5" />,
+    title: 'Quản lý thành viên',
+    url: '/manage/member',
+    icon: <IconUsers className="size-5" />,
+  },
+  {
+    title: 'Quản lý đơn hàng',
+    url: '/manage/order',
+    icon: <IconOrder className="size-5" />,
+  },
+  {
+    title: 'Quản lý bình luận',
+    url: '/manage/comment',
+    icon: <IconComment className="size-5" />,
   },
 ]
