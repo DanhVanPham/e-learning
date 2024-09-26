ab5dd00b641b5f29bb961a28554b8d9698eebab3
DanhVanPham <Danhskipper18@gmail.com>
2024-09-26 23:36:07 +0700

    fix style notification order

--- src/app/(dashboard)/order/[code]/page.tsx
+++ src/app/(dashboard)/order/[code]/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { getOrderDetails } from "@/lib/actions/order.actions";
 import PageNotFound from "@/app/not-found";
 import { formatVndPrice } from "@/utils/helpers";
+import Link from "next/link";
 
 const OrderDetails = async ({ params }: { params: { code: string } }) => {
   const code = params.code;
@@ -11,19 +12,59 @@ const OrderDetails = async ({ params }: { params: { code: string } }) => {
   if (!orderDetails) return <PageNotFound />;
 
   return (
-    <div className="flex flex-col gap-5">
-      <p>
-        Cảm ơn bạn đã mua khóa học{" "}
-        <strong className="text-primary">{orderDetails.course.title}</strong>{" "}
-        với số tiền là{" "}
-        <strong className="text-primary">
-          {formatVndPrice(orderDetails.total)}
-        </strong>
-      </p>
-      <p>
-        Bạn vui lòng thanh toán theo thông tin tài khoản dưới đây với nội dung{" "}
-        <strong className="text-primary">{orderDetails.code}</strong>
-      </p>
+    <div className="bg-white rounded-lg border bgDarkMode borderDarkMode p-5 flex text-sm lg:text-base flex-col gap-3 font-medium">
+      <div>
+        Cảm ơn bạn đã đặt mua khóa học{" "}
+        <strong className="text-primary">{orderDetails.course.title}</strong>.
+      </div>
+      <div>
+        Bạn vui lòng thanh toán vào thông tin tài khoản dưới đây với nội dung
+        chuyển khoản là{" "}
+        <strong className="text-secondary">{orderDetails.code}</strong>
+      </div>
+      <div className="max-w-[400px]">
+        <div className="relative w-full rounded-lg overflow-auto border borderDarkMode">
+          <table className="w-full caption-bottom text-xs lg:text-sm">
+            <tbody className="[&_tr:last-child]:border-0">
+              <tr className="border-b border-slate-200 dark:border-opacity-10 h-12">
+                <td className="p-4 align-middle">Số tài khoản</td>
+                <td className="p-4 align-middle">
+                  <strong>xxxxxxx</strong>
+                </td>
+              </tr>
+              <tr className="border-b border-slate-200 dark:border-opacity-10 h-12">
+                <td className="p-4 align-middle">Tên tài khoản</td>
+                <td className="p-4 align-middle">
+                  <strong>Pham Van Danh</strong>
+                </td>
+              </tr>
+              <tr className="border-b border-slate-200 dark:border-opacity-10 h-12">
+                <td className="p-4 align-middle">Ngân hàng</td>
+                <td className="p-4 align-middle">
+                  <strong>Ngân hàng Vietcombank</strong>
+                </td>
+              </tr>
+              <tr className="border-b border-slate-200 dark:border-opacity-10 h-12">
+                <td className="p-4 align-middle">Số tiền cần thanh toán</td>
+                <td className="p-4 align-middle">
+                  <strong className="text-secondary">
+                    {formatVndPrice(orderDetails.total)}
+                  </strong>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <div>
+        Nếu bạn cần hỗ trợ, vui lòng liên hệ Admin qua fb cá nhân:{" "}
+        <Link
+          className="text-primary cursor-pointer hover:underline"
+          href="https://www.facebook.com/pham.vn.7"
+        >
+          Danh_pham
+        </Link>
+      </div>
     </div>
   );
 };
